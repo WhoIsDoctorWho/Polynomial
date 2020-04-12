b1b393b48192a99635349f32e7a6d3aa8089baa9
timchnk <timchda@gmail.com>
2020-04-12 13:26:25 +0300

    Prototype created

---
+++ Polynom/Polynomial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Polynom
+{
+    public class Polynomial
+    {
+        public double[] Coefficients { get; set; } // maybe should use list
+        public double this[int degree]
+        {
+            get => Coefficients[degree];
+            set => Coefficients[degree] = value;
+        }
+        public Polynomial() {}
+        public Polynomial(int degree)
+        {
+            Coefficients = new double[degree + 1];
+        }
+        public Polynomial(double[] coefficients)
+        {
+            Coefficients = coefficients;
+        }
+        public static Polynomial operator +(Polynomial p1, Polynomial p2)
+        {
+            Polynomial result = new Polynomial();
+
+            throw new NotImplementedException();
+
+            return result;
+        }
+        public static Polynomial operator -(Polynomial p1, Polynomial p2)
+        {
+            Polynomial result = new Polynomial();
+
+            throw new NotImplementedException();
+
+            return result;
+        }
+        public static Polynomial operator *(Polynomial p1, Polynomial p2)
+        {
+            Polynomial result = new Polynomial();
+
+            throw new NotImplementedException();
+
+            return result;
+        }
+        public static Polynomial operator *(Polynomial p1, double number)
+        {
+            Polynomial result = new Polynomial();
+
+            throw new NotImplementedException();
+
+            return result;
+        }
+        public static Polynomial operator *(double number, Polynomial p1)
+        {
+            throw new NotImplementedException();
+            return p1 * number;
+        }
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
